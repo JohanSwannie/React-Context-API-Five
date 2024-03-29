04f21fb2b775ecfd9e932efcc224d2bf43508027
Johan Swan <jswannieman@gmail.com>
2024-03-29 17:26:26 +1300

    Commit TodoItem JSX Component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
 
-function TodoItem() {
-  return <div>TodoItem</div>;
+function TodoItem({ todo }) {
+  const [isTodoEditable, setIsTodoEditable] = useState(false);
+  const [todoMsg, setTodoMsg] = useState(todo.todo);
+
+  const { updateTodo, deleteTodo, toggleComplete } = useTodo();
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+    setIsTodoEditable(false);
+  };
+
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
+
+  return (
+    <div
+      className={`flex border border-black/10 rounded-lg px-2 py-1 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+        todo.completed ? "bg-[#70af55]" : "bg-[#84caa4]"
+      }`}
+    >
+      <input
+        type="checkbox"
+        className="cursor-pointer"
+        checked={todo.completed}
+        onChange={toggleCompleted}
+      />
+      <input
+        type="text"
+        className={`border outline-none w-full bg-transparent rounded-lg ${
+          isTodoEditable ? "border-white px-2 py-1" : "border-transparent"
+        }`}
+        value={todoMsg}
+        onChange={(e) => setTodoMsg(e.target.value)}
+        readOnly={!isTodoEditable}
+      />
+      <button
+        className="inline-flex w-8 h-8 rounded-lg text-sm border-black border-2 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+        onClick={() => {
+          if (todo.completed) return;
+          if (isTodoEditable) {
+            editTodo();
+          } else setIsTodoEditable((prev) => !prev);
+        }}
+        disabled={todo.completed}
+      >
+        {isTodoEditable ? "📁" : "✏️"}
+      </button>
+      <button
+        className="inline-flex w-8 h-8 rounded-lg text-sm justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 border-black border-2"
+        onClick={() => deleteTodo(todo.id)}
+      >
+        ❌
+      </button>
+    </div>
+  );
 }
 
 export default TodoItem;
